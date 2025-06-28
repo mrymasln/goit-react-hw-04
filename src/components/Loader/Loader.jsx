af45b7c09a03d9feff391393f5794699f5e66b27
meryemaslan <mrymasln25@gmail.com>
2025-06-28 09:01:24 +0300

    loader

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -1,10 +1,10 @@
-import { Circles } from "react-loader-spinner";
+import { ClipLoader } from "react-spinners";
 import styles from "./Loader.module.css";
 
 const Loader = () => {
   return (
-    <div className={styles.loader}>
-      <Circles color="#3f51b6" height={80} width={80} />
+    <div className={styles.loaderWrapper}>
+      <ClipLoader size={50} color="#3f51b5" />
     </div>
   );
 };
